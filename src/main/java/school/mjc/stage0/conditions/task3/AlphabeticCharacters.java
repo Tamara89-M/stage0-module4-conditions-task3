eead442161dfecdb02b74590fb36dbb749776d0d
Tamara89-M <tamara.muradyan04@gmail.com>
2022-07-23 19:38:44 +0400

    stage0-module4-conditions-task3

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,24 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if ((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z')) {
+            if (character == 97) {
+                System.out.println("Vowel");
+            } else if (character == 101) {
+                System.out.println("Vowel");
+            } else if (character == 105) {
+                System.out.println("Vowel");
+            } else if (character == 111) {
+                System.out.println("Vowel");
+            } else if (character == 117) {
+                System.out.println("Vowel");
+            } else if (character == 121) {
+                System.out.println("Vowel");
+            } else {
+                System.out.println("Consonant");
+            }
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,25 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double salary_variable = salary;
+        if (salary < 0){
+            System.out.println("wrong input!");
+        }else if (salary <= 10000) {
+            salary_variable -= salary_variable * 15/100;
+            System.out.println(salary_variable);
+        } else if (salary > 10000 && salary <= 20000) {
+
+            salary_variable -= salary_variable * 18 / 100;
+            System.out.println(salary_variable);
+        } else {
+
+            salary_variable -= salary_variable * 20 / 100;
+            System.out.println(salary_variable);
+        }
+    }
+
+    public static void main(String[] args) {
+        SalaryCalculator s = new SalaryCalculator();
+        s.calculateSalary(35000);
     }
 }
